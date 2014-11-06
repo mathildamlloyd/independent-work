16c9f1ad84c5be27efca84d745b920d39f9479da
Andrew Liu <zeyuliu@gmail.com>
2014-11-06 01:44:20 -0800

    updated spam model and util function

--- iw/spam/utils.py
+++ iw/spam/utils.py
@@ -1,7 +1,7 @@
 from spam.models import Word
 
-def add_word(word, is_spam, file_name):
+def add_word(word, is_spam, file_name, index):
     # See if it is already in the db
-    w, created = Word.objects.get_or_create(word=word, is_spam=is_spam, email_file=file_name)
+    w, created = Word.objects.get_or_create(word=word, is_spam=is_spam, email_file=file_name, index=index)
     if created:
         w.save()
