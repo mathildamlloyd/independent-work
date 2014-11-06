2ee342b4e0e275138a98ed50322a0d7f11ae4c61
Andrew Liu <zeyuliu@gmail.com>
2014-11-06 02:01:18 -0800

    updated db

--- iw/spam/CSDMC2010_SPAM/EmailHTMLToText.py
+++ iw/spam/CSDMC2010_SPAM/EmailHTMLToText.py
@@ -2,34 +2,38 @@ import os
 import sys
 import string
 from bs4 import BeautifulSoup
+from spam.utils import add_word
 
 def filter_func(char):
-	return char == '\n' or 32 <= ord(char) <= 126
+    return char == '\n' or 32 <= ord(char) <= 126
 
-print 'Input source directory: ' #ask for source and dest dirs
-srcdir = raw_input()
-if not os.path.exists(srcdir):
+
+def parse_emails():
+    print 'Input source directory: ' #ask for source and dest dirs
+    srcdir = raw_input()
+    if not os.path.exists(srcdir):
         print 'The source directory %s does not exist, exit...' % (srcdir)
         sys.exit()
-# dstdir is the directory where the content .eml are stored
-print 'Input destination directory: ' #ask for source and dest dirs
-dstdir = raw_input()
-if not os.path.exists(dstdir):
+    # dstdir is the directory where the content .eml are stored
+    print 'Input destination directory: ' #ask for source and dest dirs
+    dstdir = raw_input()
+    if not os.path.exists(dstdir):
         print 'The destination directory is newly created.'
         os.makedirs(dstdir)
 
-for filename in os.listdir(srcdir):
-	f = os.path.join(srcdir, filename)
-	text = ""
-	with open(f) as openfileobject:
-		for line in openfileobject:
-			text += line
-	text = filter(lambda x: x in string.printable, text)
-	parsed_text = BeautifulSoup(text).get_text()
-	parsed_text = filter(filter_func, parsed_text).lower()
-	parsed_text = parsed_text.replace("\n", " ")
-	parsed_text = parsed_text.split(" ")
-	dstfile = open(os.path.join(dstdir, filename), 'w')
-        dstfile.write(parsed_text)
-        dstfile.close()
-
+    for filename in os.listdir(srcdir):
+        f = os.path.join(srcdir, filename)
+        text = ""
+        with open(f) as openfileobject:
+            for idx, line in enumerate(openfileobject):
+                # Skip the header
+                if idx <= 3:
+                    pass
+                text += line
+        text = filter(lambda x: x in string.printable, text)
+        parsed_text = BeautifulSoup(text).get_text()
+        parsed_text = filter(filter_func, parsed_text).lower()
+        parsed_text = parsed_text.replace("\n", " ")
+        parsed_text = parsed_text.split(" ")
+        for index, word in enumerate(parsed_text):
+            add_word(word, True, f, index)
