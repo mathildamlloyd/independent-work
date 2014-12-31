25f6faa99fa8c8ee883780c58af01ee821d8ae08
Mathilda Lloyd <mathildamlloyd@gmail.com>
2014-12-31 05:18:09 +0000

    fixed bug with database word field

--- iw/spam/utils.py
+++ iw/spam/utils.py
@@ -3,12 +3,12 @@ from spam.models import Email
 from django.db.models import Sum
 
 def add_word(word, email):
-    w = Word.objects.get_or_create(word=word)
+    w = Word.objects.get_or_create(word=word)[0]
     w.emails.add(email)
     w.save()
 
 def add_email(filename, is_spam, raw_text):
-    e = Email.objects.get_or_create(email=filename, is_spam=is_spam, body=" ".join(raw_text))
-    for word in enumerate(raw_text):
+    e = Email.objects.get_or_create(email=filename, is_spam=is_spam, body=" ".join(raw_text))[0]
+    for word in raw_text:
         add_word(word, e)
     e.save()
